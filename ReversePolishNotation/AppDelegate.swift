f0ef455d1c591f595003e126fda6c666b97d16a8
Rui Peres <sauron.is.alive@gmail.com>
2014-07-12 20:28:18 +0100

    first commit

---
+++ ReversePolishNotation/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ReversePolishNotation
+//
+//  Created by Rui Peres on 12/07/2014.
+//  Copyright (c) 2014 RP. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
